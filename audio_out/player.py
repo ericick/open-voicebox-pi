bb26ae4373c03c0bb6b4a5b0af13573feddd108d
Eric Carter <ericcarter_ac@163.com>
2025-06-24 07:47:12 +0800

    Update player.py

--- audio_out/player.py
+++ audio_out/player.py
@@ -4,7 +4,8 @@ from utils.logger import logger
 def play_audio(file_path, volume=None, device=None):
     try:
         logger.debug(f"准备播放音频: {file_path}")
-        subprocess.run(['mpg123', '-q', file_path])
+        cmd = ['mpg123', '-q', '-a', device, file_path]
+        subprocess.run(cmd)
         logger.info(f"播放音频完成: {file_path}")
     except Exception as e:
         logger.error(f"播放失败: {e}")
