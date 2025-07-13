f0630661b15600087c3f9be0d24171bf6bdef004
Eric Carter <ericcarter_ac@163.com>
2025-07-13 12:23:31 +0800

    Update player.py

--- audio_out/player.py
+++ audio_out/player.py
@@ -1,6 +1,17 @@
+import subprocess
 import sounddevice as sd
 import numpy as np
 from utils.logger import logger
+
+def play_audio(file_path, volume=None, device="plughw:3,0"):
+    """播放本地音频文件（mp3/wav）"""
+    try:
+        logger.debug(f"准备播放音频: {file_path}")
+        cmd = ['mpg123', '-q', '-a', device, file_path]
+        subprocess.run(cmd)
+        logger.info(f"播放音频完成: {file_path}")
+    except Exception as e:
+        logger.error(f"播放失败: {e}")
 
 def play_audio_stream(audio_generator, samplerate=16000, channels=1, dtype='int16', device=None):
     """
