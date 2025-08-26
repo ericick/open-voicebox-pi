9c94e50e89a8b009c64bef00025deaf656babb92
Eric Carter <ericcarter_ac@163.com>
2025-08-26 21:07:32 +0800

    Update recorder.py

--- audio_in/recorder.py
+++ audio_in/recorder.py
@@ -5,7 +5,7 @@ from utils.logger import logger
 import audio_out.player as player
 
 class Recorder:
-    def __init__(self, samplerate=16000, channels=6, dtype='int16', block_size=1280, max_record_time=15, device=1):
+    def __init__(self, samplerate=16000, channels=6, dtype='int16', block_size=1280, max_record_time=15, silence_threshold=2000, silence_duration=2.0, device=1):
         self.samplerate = samplerate
         self.channels = channels
         self.dtype = dtype
@@ -13,7 +13,7 @@ class Recorder:
         self.max_record_time =  max_record_time
         self.device = device
 
-    def record_stream(self, max_record_time=15, silence_threshold=2000, silence_duration=2.0):
+    def record_stream(self):
         player.wait_until_idle(timeout_s=10)
         total_samples = int(self.samplerate * max_record_time)
         silence_chunk = int(self.samplerate * silence_duration)
